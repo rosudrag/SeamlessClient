39573fdbc3cd1bb91514427c65bcbf8773b2c6af
Garrett <52760019+Casimir255@users.noreply.github.com>
2023-12-17 21:20:14 -0600

    Update Version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.0.1")] //Set these both to the same
-[assembly: AssemblyFileVersion("2.0.0.1")]
+[assembly: AssemblyVersion("2.0.0.3")] //Set these both to the same
+[assembly: AssemblyFileVersion("2.0.0.3")]
